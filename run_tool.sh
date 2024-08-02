7e8e8c7d2ad9bdae6763336929b06a1e1b0abdf3
dmanganti <dannmanganti@gmail.com>
2024-08-01 19:19:46 -0700

    made changes to run_tool.sh

--- run_tool.sh
+++ run_tool.sh
@@ -2,12 +2,12 @@
 
 # Function to display a message and run a Python script
 run_python_script() {
-    python3 mainMenu.py
+    python mainMenu.py
 }
 
 # Function to set up the database (if not already done)
 setup_database() {
-    python3 -c "from db_connection import skeletondb; skeletondb()"
+    python -c "from db_connection import skeletondb; skeletondb()"
 }
 
 # Ensure the database is set up
